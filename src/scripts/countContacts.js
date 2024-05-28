02b377fe0d0baafc324e5c769375962d25fe763f
VovaKyrychuk <kyrychuk81@gmail.com>
2024-05-29 00:27:50 +0300

    add removeAll

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,5 +1,17 @@
+import fs from 'fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const countContacts = async () => {};
+// Функція для підрахунку кількості контактів
+export const countContacts = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf8');
+    const contacts = JSON.parse(data);
+    const count = contacts.length;
+    return count;
+  } catch (error) {
+    console.error('Помилка при підрахунку кількості контактів:', error);
+    return 0;
+  }
+};
 
 console.log(await countContacts());
--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,5 +1,15 @@
+import fs from 'fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const removeAllContacts = async () => {};
+// Функція для видалення всіх контактів
+export const removeAllContacts = async () => {
+  try {
+    // Очищення вмісту файлу db.json
+    await fs.writeFile(PATH_DB, '[]', 'utf8');
+    console.log('Усі контакти були успішно видалені.');
+  } catch (error) {
+    console.error('Помилка при видаленні контактів:', error);
+  }
+};
 
 await removeAllContacts();
